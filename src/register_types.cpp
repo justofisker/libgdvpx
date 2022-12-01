604b929a5ca1fc9f6962b9cd036f26f455794ffa
Just <37525404+justofisker@users.noreply.github.com>
2022-11-30 19:05:52 -0800

    Set/Get file functions

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -11,24 +11,20 @@
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include "example.h"
+#include "video_stream_vpx.h"
 #include "tests.h"
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_vpx_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 
-	ClassDB::register_class<ExampleRef>();
-	ClassDB::register_class<ExampleMin>();
-	ClassDB::register_class<Example>();
-	ClassDB::register_class<ExampleVirtual>(true);
-	ClassDB::register_abstract_class<ExampleAbstract>();
+	ClassDB::register_class<VideoStreamVPX>();
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
+void uninitialize_vpx_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -39,8 +35,8 @@ extern "C" {
 GDNativeBool GDN_EXPORT example_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
 	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
-	init_obj.register_initializer(initialize_example_module);
-	init_obj.register_terminator(uninitialize_example_module);
+	init_obj.register_initializer(initialize_vpx_module);
+	init_obj.register_terminator(uninitialize_vpx_module);
 	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	return init_obj.init();
